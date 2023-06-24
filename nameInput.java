6a9dadc91e3df3e6bec967e627eaac59639cadbb
Asmi Mondal <asmimondal@gmail.com>
2023-06-24 18:28:27 +0530

    New program

---
+++ nameInput.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class nameInput {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        System.out.println(name);
+    }
+}
