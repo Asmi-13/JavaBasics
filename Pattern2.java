043be5dd6991c39376960a6a0af7c466a6a3120d
Asmi Mondal <asmimondal@gmail.com>
2023-06-28 17:11:16 +0530

    First commit

--- Pattern2.java
+++ Pattern2.java
@@ -3,7 +3,15 @@ import java.util.Scanner;
 public class Pattern2{
     public static void main(int n){
         Scanner sc = new Scanner(System.in);
-        
-
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i==0 || j==0 || i==n-1 || j==n-1)
+                System.out.print(" * ");
+                else
+                System.out.print(" ");
+            }
+            System.out.ptintln();
+        }
     }
-}
+ }
